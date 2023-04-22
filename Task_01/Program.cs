e2b39786d14ea9f38e002a0a72895eb54a29885a
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-22 21:06:54 +0300

    Added Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное число - первое");
+    Console.WriteLine("Минимальное число - второе");
+}
+
+
+
+if (numberB > numberA)
+{
+    Console.WriteLine("Максимальное число - второе");
+    Console.WriteLine("Минимальное число - первое");
+}
