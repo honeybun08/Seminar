68424d3c0392aff2b22bec44f6a8e2a1ab3b95a5
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-23 16:08:51 +0300

    Added Task 08

---
+++ Task_08/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+while (count <= number)
+
+{
+    Console.Write($"{count} " );
+    count = count + 2;
+}
