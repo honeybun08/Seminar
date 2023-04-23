68e8ce9ea7f2a50fca1d3286eaaa87bdfb4ce412
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-23 15:48:01 +0300

    Added Task 06

---
+++ Task_06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
