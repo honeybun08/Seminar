71028fb9bc0b36566561e397c4775cdfcf67b727
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-25 17:21:40 +0300

    Added Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 1000) // 999 + 1, but we do not take 1000
+{
+    int result = ShowThirdDigit(number);
+    Console.WriteLine($"Третья цифра числа -> {result}");
+}
+else Console.WriteLine("Третьей цифры нет");
+
+int ShowThirdDigit (int num)
+{
+    int thirdDigit = num % 10;
+    return thirdDigit;
+}
