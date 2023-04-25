3e5b391c262982925a184632e58684bd032a1f30
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-25 18:08:30 +0300

    Added Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите цифру");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <= 7) // day of the week or not
+{
+  bool result = IsWeekend(number);
+  Console.WriteLine (result ? "Да" : "Нет");
+}
+
+else Console.WriteLine("Неккоректный ввод");
+
+bool IsWeekend (int num) // weekend or not
+{
+    return num ==6 || num == 7;
+}
+
+
+
