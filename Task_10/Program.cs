c9296bf04617f75c2ce4e430c21835ef594a18d2
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-25 16:57:25 +0300

    Added Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int result = RemoveSecondDigit(number);
+Console.WriteLine($"{number} - > {result}");
+
+
+int RemoveSecondDigit (int num)
+{
+    int needDigit = (num / 10) % 10;
+    return needDigit;
+}
+
+
+
