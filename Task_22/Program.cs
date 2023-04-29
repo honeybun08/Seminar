a69de03f360b4320c8ba1fca2483d179aa0a52d5
Ulyana <ulyana_yezhgurova@mail.ru>
2023-04-29 20:30:49 +0300

    Added Task 22

---
+++ Task_22/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите положительное целое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Square(numberN);
+
+void Square(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count, 3}  {count * count,5}");
+        count++;
+    }
+}
+
