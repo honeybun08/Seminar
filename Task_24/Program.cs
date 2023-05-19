afb2566a15d9542f4401a6fca7b05f2d75f1f12e
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-19 21:07:51 +0300

    Added Task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sumNumbers} ");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
