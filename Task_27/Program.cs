bbaf0b4dad5f481c4b9e1e6c1582266d8638e409
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-19 21:05:49 +0300

    Added Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите положительнео число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countNumbers = CountNumbers(number);
+Console.WriteLine($"Количесво цифр в числе {number} - {countNumbers}");
+
+
+int CountNumbers (int num)
+{
+    int i = 0;
+    while (num > 0)
+    {
+        i =  i + num % 10;
+        num = num / 10;
+    }
+    return i;
+}
+
