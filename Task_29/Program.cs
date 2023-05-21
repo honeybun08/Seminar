736c5bed5e1a08a77a6fe2fad8c40fc97a2bcb15
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-21 19:17:58 +0300

    Added Task 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] arr = new int[8];
+
+FillArray(arr);
+Console.Write("[");
+PrintArray(arr); 
+Console.Write("]");
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    
+}
+
+void FillArray(int[] array)
+{
+    var rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(2, 6);
+    }
+}
+
+
