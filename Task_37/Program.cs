7320a00dfcbfdb918bdd67e1ea5cf94451650544
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-23 21:11:18 +0300

    Added Task 37

---
+++ Task_37/Program.cs
@@ -0,0 +1,46 @@
+﻿int[] CreateArrayRndInt( int size, int min, int max)
+{
+    int[] arr = new int[size];   //i = index
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+int [] MultiplicationPairNums(int [] arr)
+{
+    int size = arr.Length/2;
+    if(arr.Length % 2 == 1) size +=1;
+    int [] newArr = new int [size];
+    for (int i = 0; i < arr.Length/2; i++)
+    {
+        newArr [i] = arr[i] *  arr[arr.Length - 1 -i];
+    }
+    if(arr.Length % 2 == 1) newArr[newArr.Length -1] = arr[arr.Length / 2];
+    return newArr;
+}
+
+
+int [] array = CreateArrayRndInt(5, 1, 10);
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+int [] resArr = MultiplicationPairNums(array);
+
+Console.Write("[");
+PrintArray(resArr, ",");
+Console.WriteLine("]");
