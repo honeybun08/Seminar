5456bae3af1be2e403633087d432ffbf2e5c702d
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-23 21:10:27 +0300

    Added Task 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] CreateArrayRndInt( int size, int min, int max)
+{
+    int[] arr = new int[size];   //i = index
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
+int[] ElemCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+        
+    }
+
+    return new int[] {count};
+}
+
+
+
+
+int [] array = CreateArrayRndInt(4, 100, 999);
+
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+
+
+int [] elemCount = ElemCount(array);
+Console.WriteLine($"Количество четных чисел в массиве равно {elemCount[0]}");
+
