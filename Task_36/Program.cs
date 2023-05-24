42bcaaf56e4daa37f5ba68554382626a62307d8f
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-24 17:27:47 +0300

    Added Task 36

---
+++ Task_36/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] CreateArrayRndInt( int size, int min, int max)
+{
+    int[] arr = new int[size];   //i = index
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
+int GetSumNegativePosElem(int[] arr)
+{
+    int sum = 0;
+    int i = 1;
+    while (i < arr.Length)   
+    {
+        sum += arr[i];
+        i = i + 2;
+    }
+    return sum;
+}
+
+int [] array = CreateArrayRndInt(4, 3, 29);
+Console.Write("[");
+PrintArray(array, ",");
+Console.WriteLine("]");
+
+
+int sum = GetSumNegativePosElem(array);
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях в массиве равно {sum}");
+
