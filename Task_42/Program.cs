bb90da037d77d00a2daefa5a7cdbba87e5136823
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-25 22:29:36 +0300

    Added Task 42

---
+++ Task_42/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: "); // перевод числа в двоичную систему
+int number = Convert.ToInt32(Console.ReadLine());
+
+string result = DecToBin(number);
+Console.WriteLine(result);
+
+string DecToBin(int num)
+{
+    string res = string.Empty;  //res = result
+    while (num > 0)
+    {
+        res = num % 2 + res; //если +рес после нам, то оно переварачивается
+        num = num / 2;   
+    }
+    return res;
+}
