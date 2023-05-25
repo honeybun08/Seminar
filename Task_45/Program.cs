5bcbf6c1f76391566c767d7dcf9ff8d24c79f438
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-25 22:30:19 +0300

    Added Task 45

---
+++ Task_45/Program.cs
@@ -0,0 +1,41 @@
+﻿// поэлементное копирование массива
+
+int[] oldArray = CreateArrayRndInt(5,10,20);
+PrintArray(oldArray);
+Console.WriteLine();
+int[] newArray = CopyArray(oldArray);
+PrintArray(newArray);
+
+int[] CreateArrayRndInt( int size, int min, int max)
+{
+    int[] arr = new int[size];   //i = index
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr, string sep = ",")
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}{sep} ");
+        else Console.Write($"{arr[i]}");
+    }
+}
+
+
+
+int[] CopyArray (int[] arr)
+{
+    int[] newarray = new int[arr.Length]; // размер = копия исходного размера
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newarray[i] = arr[i];
+    }
+    return newarray;
+}
