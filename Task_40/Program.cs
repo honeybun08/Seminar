705b301d1b9b5b67029db9bff521eab0a599bc2f
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-25 22:29:15 +0300

    Added Task 40

---
+++ Task_40/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первую сторону ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую сторону ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третью сторону ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool isTriangle(int numA, int numB, int numC)
+{
+    int sum = numB + numC;
+    if (numA < sum) return true;
+    else return false;
+}
+
+bool IsTriangle = isTriangle(numberA,numberB,numberC);
+Console.WriteLine (IsTriangle ?  "Такой треугольник существует" : "Такой треугольник не существует");
