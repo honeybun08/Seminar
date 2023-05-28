6daa354ad1be72509f5d8cdb2bbdc33eff671ce7
Ulyana <ulyana_yezhgurova@mail.ru>
2023-05-28 14:02:06 +0300

    Added Task 43

---
+++ Task_43/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);   // у меня метод совершенно не получился((
+double y = k1 * x + b1;
+
+Console.WriteLine($"Две прямые пересекутся в точке с координаатми X: {x}, Y: {y}");
+
+
+
