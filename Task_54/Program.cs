7e8fe9e3b1ecc081f909d5b7e550911d674df218
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-04 12:54:39 +0300

    Added Task 54

---
+++ Task_54/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию
+//элементы каждой строки двумерного массива
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)      
+{
+    int[,] matrix = new int[rows, columns];                              
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)             
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)           
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)           
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSortRowsToLower(int [,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)  // k - последний столбец и начинает с последнего столбца
+            {
+                if (matrix[i,k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i,k] = temp;;
+                }
+            }
+        }
+    }
+}
+
+int [,] array2d = CreateMatrixRndInt(3, 4, 1, 9 );
+PrintMatrix(array2d);
+Console.WriteLine();
+BubbleSortRowsToLower(array2d);
+PrintMatrix(array2d);
+
