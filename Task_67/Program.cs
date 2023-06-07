3a83f477dbe202b48d477d99ef6a67b7d9713262
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-07 13:09:56 +0300

    Added Task 67

---
+++ Task_67/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает число, выводит сумму его цифр
+
+Console.WriteLine("Введите натуральное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumRec(number));
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumRec(n / 10);
+}             // sec move       // first move
+
