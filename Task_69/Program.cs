5f9bc367bcf1d1d5f3637890b03824b2c3fc4e94
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-07 13:12:35 +0300

    Added Task 69

---
+++ Task_69/Program.cs
@@ -0,0 +1,16 @@
+﻿// Чисо А в натуральной степени B
+
+Console.WriteLine("Введите натуральное число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SquareRec(numberA, numberB));
+
+int SquareRec(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * SquareRec(a, b - 1);    // степень уменьшается пока не дойдет до 0
+}                           //2 параметра
+
