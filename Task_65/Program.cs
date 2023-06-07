9cc453dbd3eceecab36930401a2d8ca1e625708b
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-07 13:07:00 +0300

    Added Task 65

---
+++ Task_65/Program.cs
@@ -0,0 +1,28 @@
+﻿// числа в промежутке от M до N(по возрастанию или убыванию)
+
+Console.WriteLine("Введите натуральное число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersRange(numberM, numberN);
+
+void NaturalNumbersRange(int numM, int numN)
+{
+    if (numM  > numN)
+    {
+    Console.Write($" {numM}");
+    NaturalNumbersRange(numM - 1, numN);    // 5, 4, 3...
+    }
+    else if (numM < numN)
+    {
+    Console.Write($" {numM}");
+    NaturalNumbersRange(numM + 1, numN);   // 1, 2, 3...
+    }
+    else Console.Write($" {numM}");
+    
+}
+
+
+
