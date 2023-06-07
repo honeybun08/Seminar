42bc728c5b200367fec22ba742ae4136b7f5f4f8
Ulyana <ulyana_yezhgurova@mail.ru>
2023-06-07 13:04:02 +0300

    Added Task 63

---
+++ Task_63/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от 1 до N.
+// 4 -> " 1, 2, 3, 4"
+// 2 -> " , 1, 2"
+
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+
+
+void NaturalNumbers(int num)                               
+{
+    // условие выхода, идем наоборот, когда num = 0
+    if (num == 0) return;
+    NaturalNumbers(num - 1) ;                   // должно уменьшатся
+    Console.Write($"{num} ");
+}
